6e8f779c560566252646a8eb0492a6f57a0b2134
elbur27 <mamatzhanov1397@gmail.com>
2024-11-07 09:51:38 +0600

    attems to develop frontend

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -26,4 +27,6 @@ urlpatterns = [
     path('docs<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
